6c96407b13c74fb1ce7ed45990c7378a6b3400f8
HuberPascal <136008239+HuberPascal@users.noreply.github.com>
2025-06-08 15:25:44 +0200

    feat: Implement routing

--- client/webapp/src/app/app.routes.ts
+++ client/webapp/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { PizzaListComponent } from './components/pizza-list/pizza-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/pizzas', pathMatch: 'full' },
+  { path: 'pizzas', component: PizzaListComponent },
+];
