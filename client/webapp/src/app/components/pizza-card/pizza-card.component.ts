ce336627817dea89a4e909d5774dc0812d9c3905
HuberPascal <136008239+HuberPascal@users.noreply.github.com>
2025-06-08 15:26:13 +0200

    feat: Implement basic layout components

---
+++ client/webapp/src/app/components/pizza-card/pizza-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pizza-card',
+  imports: [],
+  templateUrl: './pizza-card.component.html',
+  styleUrl: './pizza-card.component.scss'
+})
+export class PizzaCardComponent {
+
+}
