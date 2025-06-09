ad108c2d166c0e346a6ec379c648924478d61e73
HuberPascal <136008239+HuberPascal@users.noreply.github.com>
2025-06-09 16:08:46 +0200

    feat: Implement pizza list

--- client/webapp/src/app/components/pizza-list/pizza-list.component.ts
+++ client/webapp/src/app/components/pizza-list/pizza-list.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { CustomChipComponent } from '../custom-chip/custom-chip.component';
 
 @Component({
   selector: 'app-pizza-list',
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    MatChipsModule,
+    MatButtonToggleModule,
+    CustomChipComponent,
+  ],
   templateUrl: './pizza-list.component.html',
-  styleUrl: './pizza-list.component.scss'
+  styleUrl: './pizza-list.component.scss',
 })
 export class PizzaListComponent {
+  @Input() options: { label: string; value: any }[] = [
+    { label: 'alle', value: 1 },
+    { label: 'pizzas', value: 2 },
+  ];
+  @Input() selected: any;
+  @Output() selectionChange = new EventEmitter<any>();
 
+  onSelect(value: any) {
+    this.selected = value;
+    this.selectionChange.emit(value);
+  }
 }
