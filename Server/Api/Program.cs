7c9b4a5bfe92ca35109dc83aa7ef14a794ec4374
HuberPascal <136008239+HuberPascal@users.noreply.github.com>
2025-06-15 17:35:07 +0200

    fix: configure CORS to allow Angular app access

--- Server/Api/Program.cs
+++ Server/Api/Program.cs
@@ -11,6 +11,17 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngularApp", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
+    });
+});
+
 builder.Services.AddAutoMapper(typeof(OrderProfile));
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
@@ -35,6 +46,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("AllowAngularApp");
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
