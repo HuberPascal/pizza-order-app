d88f8d687037329018bde903a2d9f403c0d7adfd
HuberPascal <136008239+HuberPascal@users.noreply.github.com>
2025-06-19 14:12:21 +0200

    WIP: implement extra toppings selection in pizza dialog

--- client/webapp/src/app/components/pizza-list/pizza-list.component.ts
+++ client/webapp/src/app/components/pizza-list/pizza-list.component.ts
@@ -1,10 +1,14 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, effect, inject, OnInit } from '@angular/core';
 import { CustomCategoryFilterComponent } from '../custom-category-filter/custom-category-filter.component';
 import { PizzaCardComponent } from '../pizza-card/pizza-card.component';
 import { PizzaStore } from '../../stores/pizza.store';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { PizzaOrderDialogComponent } from '../pizza-order-dialog/pizza-order-dialog.component';
-import { PizzaDto } from '../../api/core/services/service-proxies';
+import {
+  ExtraToppingClient,
+  PizzaDto,
+} from '../../api/core/services/service-proxies';
+import { ExtraToppingStore } from '../../stores/extraToppings.store';
 
 @Component({
   selector: 'app-pizza-list',
@@ -14,16 +18,30 @@ import { PizzaDto } from '../../api/core/services/service-proxies';
 })
 export class PizzaListComponent implements OnInit {
   readonly store = inject(PizzaStore);
+  readonly extraToppingsStore = inject(ExtraToppingStore);
   readonly dialog = inject(MatDialog);
 
   ngOnInit() {
     this.store.loadAllPizzas();
+    this.extraToppingsStore.loadAllExtraToppings();
   }
 
-  openDialog() {
+  constructor() {
+    effect(() => {
+      console.log(
+        'ExtraToppings changed:',
+        this.extraToppingsStore.extraToppings()
+      );
+      console.log('Loading state:', this.extraToppingsStore.loading());
+      console.log('Error state:', this.extraToppingsStore.error());
+    });
+  }
+
+  openDialog(pizza: PizzaDto) {
     const dialogRef = this.dialog.open(PizzaOrderDialogComponent, {
       data: {
-        pizza: PizzaDto,
+        pizza: pizza,
+        extraToppings: this.extraToppingsStore.extraToppings(),
       },
     });
 
--- client/webapp/src/app/components/pizza-order-dialog/pizza-order-dialog.component.ts
+++ client/webapp/src/app/components/pizza-order-dialog/pizza-order-dialog.component.ts
@@ -1,20 +1,96 @@
-import { AfterViewInit, Component, inject } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  computed,
+  inject,
+  effect,
+  signal,
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CustomChipComponent } from '../custom-chip/custom-chip.component';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDividerModule } from '@angular/material/divider';
+import {
+  ExtraToppingDto,
+  PizzaDto,
+} from '../../api/core/services/service-proxies';
+
+interface ExtraTopping {
+  id: string;
+  name: string;
+  price: number;
+  selected: boolean;
+}
+
+// interface ToppingsForm {
+//   toppings: FormControl<ExtraTopping[]>; // Komplette Objekte mit selected-Flag
+// }
+
+interface ToppingsForm {
+  selectedToppings: FormControl<string[]>;
+}
 
 @Component({
   selector: 'app-pizza-order-dialog',
-  imports: [MatCardModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    CustomChipComponent,
+    MatButtonModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatDividerModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './pizza-order-dialog.component.html',
   styleUrl: './pizza-order-dialog.component.scss',
 })
 export class PizzaOrderDialogComponent implements AfterViewInit {
-  pizza = inject(MAT_DIALOG_DATA);
-
+  data: { pizza: PizzaDto; extraToppings: ExtraToppingDto[] } =
+    inject(MAT_DIALOG_DATA);
   readonly dialogRef = inject(MatDialogRef<PizzaOrderDialogComponent>);
 
+  form = new FormGroup<ToppingsForm>({
+    selectedToppings: new FormControl<string[]>([], { nonNullable: true }),
+  });
+
+  extraToppings = computed(() =>
+    this.getExtraToppingsFromDto(this.data.extraToppings ?? [])
+  );
+
+  originalFormValues = computed(() => this.getFormValuesFromDto());
+
+  constructor() {
+    effect(() => {
+      const originalValues = this.originalFormValues();
+      if (originalValues) {
+        this.form.setValue(originalValues);
+      }
+    });
+    effect(() => console.log('extratoppings: ', this.extraToppings()));
+  }
+
   ngAfterViewInit(): void {
-    console.log('dialog pizza is:', this.pizza);
+    console.log('dialog pizza is:', this.data.pizza);
+  }
+
+  getExtraToppingsFromDto(
+    extraToppingsArray: ExtraToppingDto[]
+  ): ExtraTopping[] {
+    return extraToppingsArray.map((topping) => ({
+      id: topping.id ?? '',
+      name: topping.name ?? '',
+      price: topping.price ?? 0,
+      selected: false,
+    }));
+  }
+
+  getFormValuesFromDto() {
+    return {
+      selectedToppings: [],
+    };
   }
 }
